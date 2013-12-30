b731dd3e3e0c1c0b59c9466ed59c368b0b521fc7
Shabith Ishan <shabith.ishan@gmail.com>
2013-12-30 23:34:35 +0530

    z.sh added

---
+++ .extra
@@ -0,0 +1,2 @@
+# init z   https://github.com/rupa/z
+. ~/code/z/z.sh
--- install-deps.sh
+++ install-deps.sh
@@ -5,7 +5,7 @@
 
 # https://github.com/jamiew/git-friendly
 # the `push` command which copies the github compare URL to my clipboard is heaven
-zsh < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
+bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
 
 # https://rvm.io
 # rvm for the rubiess
