6b95235a0f20685d79fdd73dbda8d2816ba08751
Shabith Ishan <shabith.ishan@gmail.com>
2017-10-22 11:35:17 +0530

    installation script related changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,7 +15,7 @@ function doIt() {
 	source ~/.bash_profile;
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
+if [[ "$1" == "--force" || "$1" == "-f" ]]; then
 	doIt;
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
@@ -25,3 +25,4 @@ else
 	fi;
 fi;
 unset doIt;
+
