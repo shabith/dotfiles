4d4d170567fddb03d9d3339d5761b55068e39899
Shabith Ishan <shabith.ishan@gmail.com>
2017-10-22 12:53:43 +0530

    remove z from deps

--- install-deps.sh
+++ install-deps.sh
@@ -22,9 +22,9 @@ npm install -g pure-prompt
 
 # https://github.com/rupa/z
 # z, oh how i love you
-cd ~/code
-git clone https://github.com/rupa/z.git
-chmod +x ~/code/z/z.sh
+# cd ~/code
+# git clone https://github.com/rupa/z.git
+# chmod +x ~/code/z/z.sh
 # also consider moving over your current .z file if possible. it's painful to rebuild :)
 
 # z binary is already referenced from .bash_profile
